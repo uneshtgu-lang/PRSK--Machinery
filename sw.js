0b4ad9db8fe9577b1be2ffe6368ba5774c6fc71a
uneshtgu-lang <uneshtgu@gmail.com>
2026-06-05 13:09:23 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE = 'prsk-v1';
+const ASSETS = ['./', './index.html', './manifest.json'];
+self.addEventListener('install', e => e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS))));
+self.addEventListener('fetch', e => e.respondWith(fetch(e.request).catch(() => caches.match(e.request))));
